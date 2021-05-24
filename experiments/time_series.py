84528a174069cb5bda0422791516cdcbc1abe68e
Jonathan Crabbé <jonathan.cr1302@gmail.com>
2021-05-24 17:51:43 +0100

    Improve time series experiment

--- experiments/time_series.py
+++ experiments/time_series.py
@@ -1,9 +1,11 @@
 from statsmodels.tsa.arima_process import arma_generate_sample
 from visualization.time_series import plot_time_series
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 from models.time_series_forecasting import TimeSeriesForecaster
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 
 class TimeSeriesDataset(torch.utils.data.Dataset):
@@ -18,37 +20,68 @@ class TimeSeriesDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
 
+
 def generate_arma(random_seed: int = 42):
-    ar_coefs = [1, .75, -.25]
-    ma_coefs = [1, .65, .35]
+    ar_coefs = [1, -.9]
+    ma_coefs = [1, .25, .15]
     np.random.seed(random_seed)
-    X = arma_generate_sample(ar_coefs, ma_coefs, nsample=(1000, 50))
-    X, y = torch.from_numpy(X[:, :-1]), torch.from_numpy(X[:, -1])
-    X = X.view(1000, 49, 1)
-    return X.float(), y.float()
+    X = arma_generate_sample(ar_coefs, ma_coefs, nsample=(10000, 31))
+    X, Y = X[:, 10:-1], X[:, 11:]
+    return X, Y
 
 
 def arma_precision(random_seed: int = 42, cv: int = 0):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.manual_seed(random_seed)
-    X, y = generate_arma(random_seed+cv)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed+cv)
-    training_set = TimeSeriesDataset(X_train, y_train)
+    X, Y = generate_arma(random_seed + cv)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=random_seed + cv)
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    X_train = scaler.fit_transform(X_train)
+    Y_train = scaler.transform(Y_train)
+    X_train = X_train.reshape(len(X_train), -1, 1)
+    X_train = torch.from_numpy(X_train).float()
+    X_test = scaler.transform(X_test)
+    Y_test = scaler.transform(Y_test)
+    X_test = X_test.reshape(len(X_test), -1, 1)
+    X_test = torch.from_numpy(X_test).float().to(device)
+    Y_train = Y_train.reshape(len(Y_train), -1, 1)
+    Y_train = torch.from_numpy(Y_train).float()
+    Y_test = Y_test.reshape(len(Y_test), -1, 1)
+    Y_test = torch.from_numpy(Y_test).float().to(device)
+    training_set = TimeSeriesDataset(X_train, Y_train)
     train_loader = torch.utils.data.DataLoader(training_set, batch_size=20, shuffle=True)
 
-    model = TimeSeriesForecaster()
-    opt = torch.optim.Adam(params=model.parameters())
+    #plot_time_series(X_train[0])
+    #plot_time_series(X_test[0])
+
+    model = TimeSeriesForecaster().to(device)
+    opt = torch.optim.Adam(params=model.parameters(), lr=1.0e-3)
+
+    print(torch.mean(torch.abs(Y_test - model(X_test))))
+    model.hidden = model.init_hidden(batch_size=1)
+    plt.plot(np.arange(0, X_test.shape[1]), X_test[0].detach().cpu().numpy())
+    plt.plot(np.arange(1, X_test.shape[1]+1), model(X_test[0:1])[0].detach().cpu().numpy())
+    plt.show()
 
     model.train()
-    for epoch in range(10):
-        for X, y in train_loader:
+    for epoch in range(100):
+        for X, Y in train_loader:
+            X = X.to(device)
+            Y = Y.to(device)
+            model.hidden = model.init_hidden(batch_size=len(X))
             opt.zero_grad()
-            y_pred = model(X)
-            error = torch.mean((y-y_pred)**2)
+            Y_pred = model(X)
+            error = torch.sum((Y - Y_pred) ** 2)
             error.backward()
             opt.step()
+        if epoch % 2 == 0:
+            model.hidden = model.init_hidden(batch_size=len(X_test))
+            print(f'Epoch {epoch+1}: Test MSE = {torch.mean((Y_test - model(X_test))**2):.3g}.')
 
-    print(torch.mean((y_test - model(X_test))**2))
-
+    model.hidden = model.init_hidden(batch_size=1)
+    plt.plot(np.arange(0, X_test.shape[1]), X_test[0].detach().cpu().numpy())
+    plt.plot(np.arange(1, X_test.shape[1] + 1), model(X_test[0:1])[0].detach().cpu().numpy())
+    plt.show()
 
 
 arma_precision()
--- models/time_series_forecasting.py
+++ models/time_series_forecasting.py
@@ -4,20 +4,54 @@ import torch.nn.functional as F
 
 
 class TimeSeriesForecaster(nn.Module):
-    def __init__(self, n_cont: int = 3):
-        super().__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=128, batch_first=True)
-        self.lin = nn.Linear(128, 1)
+    def __init__(self, input_dim: int = 1, hidden_dim: int = 100, output_dim: int = 1, num_layers: int = 2,
+                 batch_size: int = 20):
+        super(TimeSeriesForecaster, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.batch_size = batch_size
+
+        self.rnn = nn.RNN(input_size=self.input_dim, hidden_size=self.hidden_dim,
+                          batch_first=True, num_layers=self.num_layers, nonlinearity='relu')
+        self.lin = nn.Linear(hidden_dim, output_dim)
+        self.hidden = self.init_hidden(batch_size)
+        for name, param in self.rnn.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 0.0)
+            elif 'weight' in name:
+                nn.init.xavier_normal_(param)
+        '''
+        self.lstm = nn.LSTM(input_size=self.input_dim, hidden_size=self.hidden_dim,
+                            batch_first=True, num_layers=self.num_layers)
+        for name, param in self.lstm.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 0.0)
+            elif 'weight' in name:
+                nn.init.xavier_normal_(param)
+        '''
+
+    def init_hidden(self, batch_size):
+        h0, c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim),\
+                 torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+        return h0, c0
 
     def forward(self, x):
-        x = self.latent_representation(x)[0]
+        x, self.hidden = self.rnn(x)
         x = self.lin(x)
         return x
 
     def latent_representation(self, x):
-        x = self.lstm(x)
-        return x
+        x, self.hidden = self.lstm(x)
+        return x[:, -1, :]
 
     def latent_to_output(self, x):
         x = self.lin(x)
+        return x
+
+    def seq2seq(self, x):
+        x, _ = self.lstm(x)
+        x = self.lin(x)
+        print(x.shape)
         return x
